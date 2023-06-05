2b3df0b18cf4814d2ac7a1abb436e205bbc4b01c
fabi <frank.birkhofen@t-systems.com>
2023-06-05 16:30:30 +0200

    lab string-info

---
+++ lab/string-info.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+name = input("what is your name?")
+
+# print first character
+print(name[0])
+
+# print last character
+print("last character: " + name[-1])
+
+# print middle character
+print("middle character: " + name[int(len(name)/2)])
+
+# print every even character
+print("every even character: " + name[1::2])
+
+# print every odd index character
+print("every odd character: " + name[0::2])
+
+# print message in reverse
+print(name[::-1])
