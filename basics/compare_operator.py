3678657383f9208bfd42f8f1b3f83d1cb98807b3
Frank Birkhofen <frank.birkhofen@telekom.de>
2023-12-07 15:39:09 +0100

    * optimize some format things

--- basics/compare_operator.py
+++ basics/compare_operator.py
@@ -6,15 +6,14 @@
 # compare integers
 # 1 is equal to 1
 result1 = 1 == 1
-print(result1) # this is a trailing comment
+print(result1)  # this is a trailing comment
 
 # compare integers with float
 # 2 is equal to 2.0
 result2 = 2 == 2.0
-print(result2) # this is a trailing comment
-
+print(result2)  # this is a trailing comment
 
 # compare strings
 # 3string1is equal to string2
 result3 = 'string1' is not 'string3'
-print(result3) # this is a trailing comment
+print(result3)  # this is a trailing comment
